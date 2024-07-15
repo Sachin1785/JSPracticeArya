bca6908b506a2115d445ef2e387d785c8344b23b
Sachin1785 <SACHHIDANANDA.MAHAPATRO228@svkmmumbai.onmicrosoft.com>
2024-07-15 11:48:22 +0530

    initial commit

---
+++ blackjack/scripts.js
@@ -0,0 +1,42 @@
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = true
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+function rand() {
+    return Math.floor(Math.random() * 13) + 1
+}
+
+function startGame() {
+    cards = [rand(), rand()]
+    sum = cards[0] + cards[1]
+    isAlive = true
+    hasBlackJack = false
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: " + cards.join(" ")
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        messageEl.textContent = "draw a new card?"
+    } else if (sum === 21) {
+        messageEl.textContent = "Blackjack!"
+        hasBlackJack = true
+    } else {
+        messageEl.textContent = "You're out!"
+        isAlive = false
+    }
+}
+
+function newCard() {
+    if (isAlive && !hasBlackJack) {
+        let card = rand()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
+}
---
+++ increment/scrip.js
@@ -0,0 +1,19 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+console.log(saveEl)
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+    // 3. Render the variable in the saveEl using innerText
+    saveEl.textContent += countStr
+    // NB: Make sure to not delete the existing content of the paragraph
+    console.log(count)
+}
